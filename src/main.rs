0ad077bfaa94e1f72eb9a5339f0f657e9c20cc1b
Gheorghe Prelipcean <prelipcean.gheorghe@yahoo.com>
2025-07-06 11:08:29 +0300

    Code formated using: cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ mod startup_stm32f429zi; // MCU-specific startup code
 static mut COUNTER: u32 = 42;
 
 #[unsafe(no_mangle)]
-fn main()  -> ! {
+fn main() -> ! {
     // This is where our application logic will go.
 
     loop {
@@ -25,7 +25,5 @@ fn main()  -> ! {
 #[panic_handler]
 fn panic_handler(_info: &PanicInfo) -> ! {
     // Trap the MCU in an infinite loop on panic.
-    loop {
-
-    }
-} 
+    loop {}
+}
--- src/startup_stm32f429zi.rs
+++ src/startup_stm32f429zi.rs
@@ -6,11 +6,11 @@ use core::ptr;
 
 // Symbols provided by the linker script for memory initialization
 unsafe extern "C" {
-    static _sidata: u32;      // Start of init values for .data
-    static mut _sdata: u32;   // Start of .data section
-    static mut _edata: u32;   // End of .data section
-    static mut _sbss: u32;    // Start of .bss section
-    static mut _ebss: u32;    // End of .bss section
+    static _sidata: u32; // Start of init values for .data
+    static mut _sdata: u32; // Start of .data section
+    static mut _edata: u32; // End of .data section
+    static mut _sbss: u32; // Start of .bss section
+    static mut _ebss: u32; // End of .bss section
 }
 
 // Interrupt vector table
@@ -19,118 +19,116 @@ unsafe extern "C" {
 static VECTOR_TABLE: [Option<extern "C" fn()>; 107] = [
     // Initial Stack Pointer (first entry, value provided by linker script)
     None, // This entry is filled by the linker script with _start_of_stack
-
     // Core Cortex-M Exceptions (Vector Table Position 1-15)
-    Some(Reset_Handler),        // Position 1: Reset Handler (our custom entry point)
-    Some(NMI_Handler),          // Position 2: Non-Maskable Interrupt
-    Some(HardFault_Handler),    // Position 3: Hard Fault
-    Some(MemManage_Handler),    // Position 4: Memory Management Fault
-    Some(BusFault_Handler),     // Position 5: Bus Fault
-    Some(UsageFault_Handler),   // Position 6: Usage Fault
+    Some(Reset_Handler),     // Position 1: Reset Handler (our custom entry point)
+    Some(NMI_Handler),       // Position 2: Non-Maskable Interrupt
+    Some(HardFault_Handler), // Position 3: Hard Fault
+    Some(MemManage_Handler), // Position 4: Memory Management Fault
+    Some(BusFault_Handler),  // Position 5: Bus Fault
+    Some(UsageFault_Handler), // Position 6: Usage Fault
     None,
     None,
     None,
-    None,                       // Position 7-10: Reserved
-    Some(SVCall_Handler),       // Position 11: SVCall
-    Some(DebugMon_Handler),     // Position 12: Debug Monitor
-    None,                       // Position 13: Reserved
-    Some(PendSV_Handler),       // Position 14: PendSV
-    Some(SysTick_Handler),      // Position 15: SysTick
-
+    None,                   // Position 7-10: Reserved
+    Some(SVCall_Handler),   // Position 11: SVCall
+    Some(DebugMon_Handler), // Position 12: Debug Monitor
+    None,                   // Position 13: Reserved
+    Some(PendSV_Handler),   // Position 14: PendSV
+    Some(SysTick_Handler),  // Position 15: SysTick
     // STM32F429ZI Specific Peripheral Interrupts (IRQs) - starting from IRQ 0
     // These entries correspond to the IRQ numbers and their order in the STM32F429xx Reference Manual, Table 63.
     // Vector Table Position = 16 + IRQ Number
-    Some(WWDG_Handler),                 // IRQ0: Window Watchdog Interrupt
-    Some(PVD_Handler),                  // IRQ1: PVD through EXTI Line Detection Interrupt
-    Some(TAMP_STAMP_Handler),           // IRQ2: Tamper and TimeStamp Interrupts
-    Some(RTC_WKUP_Handler),             // IRQ3: RTC Wakeup Interrupt through the EXTI line
-    Some(FLASH_Handler),                // IRQ4: FLASH global Interrupt
-    Some(RCC_Handler),                  // IRQ5: RCC global Interrupt
-    Some(EXTI0_Handler),                // IRQ6: EXTI Line0 Interrupt
-    Some(EXTI1_Handler),                // IRQ7: EXTI Line1 Interrupt
-    Some(EXTI2_Handler),                // IRQ8: EXTI Line2 Interrupt
-    Some(EXTI3_Handler),                // IRQ9: EXTI Line3 Interrupt
-    Some(EXTI4_Handler),                // IRQ10: EXTI Line4 Interrupt
-    Some(DMA1_Stream0_Handler),         // IRQ11: DMA1 Stream 0 global Interrupt
-    Some(DMA1_Stream1_Handler),         // IRQ12: DMA1 Stream 1 global Interrupt
-    Some(DMA1_Stream2_Handler),         // IRQ13: DMA1 Stream 2 global Interrupt
-    Some(DMA1_Stream3_Handler),         // IRQ14: DMA1 Stream 3 global Interrupt
-    Some(DMA1_Stream4_Handler),         // IRQ15: DMA1 Stream 4 global Interrupt
-    Some(DMA1_Stream5_Handler),         // IRQ16: DMA1 Stream 5 global Interrupt
-    Some(DMA1_Stream6_Handler),         // IRQ17: DMA1 Stream 6 global Interrupt
-    Some(ADC_Handler),                  // IRQ18: ADC1, ADC2 and ADC3 global Interrupts
-    Some(CAN1_TX_Handler),              // IRQ19: CAN1 TX Interrupt
-    Some(CAN1_RX0_Handler),             // IRQ20: CAN1 RX0 Interrupt
-    Some(CAN1_RX1_Handler),             // IRQ21: CAN1 RX1 Interrupt
-    Some(CAN1_SCE_Handler),             // IRQ22: CAN1 SCE Interrupt
-    Some(EXTI9_5_Handler),              // IRQ23: EXTI Line[9:5] Interrupts
-    Some(TIM1_BRK_TIM9_Handler),        // IRQ24: TIM1 Break interrupt and TIM9 global interrupt
-    Some(TIM1_UP_TIM10_Handler),        // IRQ25: TIM1 Update Interrupt and TIM10 global interrupt
-    Some(TIM1_TRG_COM_TIM11_Handler),   // IRQ26: TIM1 Trigger and Commutation Interrupts and TIM11 global interrupt
-    Some(TIM1_CC_Handler),              // IRQ27: TIM1 Capture Compare Interrupt
-    Some(TIM2_Handler),                 // IRQ28: TIM2 global Interrupt
-    Some(TIM3_Handler),                 // IRQ29: TIM3 global Interrupt
-    Some(TIM4_Handler),                 // IRQ30: TIM4 global Interrupt
-    Some(I2C1_EV_Handler),              // IRQ31: I2C1 Event Interrupt
-    Some(I2C1_ER_Handler),              // IRQ32: I2C1 Error Interrupt
-    Some(I2C2_EV_Handler),              // IRQ33: I2C2 Event Interrupt
-    Some(I2C2_ER_Handler),              // IRQ34: I2C2 Error Interrupt
-    Some(SPI1_Handler),                 // IRQ35: SPI1 global Interrupt
-    Some(SPI2_Handler),                 // IRQ36: SPI2 global Interrupt
-    Some(USART1_Handler),               // IRQ37: USART1 global Interrupt
-    Some(USART2_Handler),               // IRQ38: USART2 global Interrupt
-    Some(USART3_Handler),               // IRQ39: USART3 global Interrupt
-    Some(EXTI15_10_Handler),            // IRQ40: EXTI Line[15:10] Interrupts
-    Some(RTC_Alarm_Handler),            // IRQ41: RTC Alarms (A and B) through EXTI Line Interrupt
-    Some(OTG_FS_WKUP_Handler),          // IRQ42: USB OTG FS Wakeup through EXTI line interrupt
-    Some(TIM8_BRK_TIM12_Handler),       // IRQ43: TIM8 Break Interrupt and TIM12 global interrupt
-    Some(TIM8_UP_TIM13_Handler),        // IRQ44: TIM8 Update Interrupt and TIM13 global interrupt
-    Some(TIM8_TRG_COM_TIM14_Handler),   // IRQ45: TIM8 Trigger and Commutation Interrupts and TIM14 global interrupt
-    Some(TIM8_CC_Handler),              // IRQ46: TIM8 Capture Compare Interrupt
-    Some(DMA1_Stream7_Handler),         // IRQ47: DMA1 Stream7 global Interrupt
-    Some(FSMC_Handler),                 // IRQ48: FSMC global Interrupt (Flexible Static Memory Controller)
-    Some(SDIO_Handler),                 // IRQ49: SDIO global Interrupt
-    Some(TIM5_Handler),                 // IRQ50: TIM5 global Interrupt
-    Some(SPI3_Handler),                 // IRQ51: SPI3 global Interrupt
-    Some(UART4_Handler),                // IRQ52: UART4 global Interrupt
-    Some(UART5_Handler),                // IRQ53: UART5 global Interrupt
-    Some(TIM6_DAC_Handler),             // IRQ54: TIM6 global Interrupt and DAC1&2 underrun errors
-    Some(TIM7_Handler),                 // IRQ55: TIM7 global Interrupt
-    Some(DMA2_Stream0_Handler),         // IRQ56: DMA2 Stream 0 global Interrupt
-    Some(DMA2_Stream1_Handler),         // IRQ57: DMA2 Stream 1 global Interrupt
-    Some(DMA2_Stream2_Handler),         // IRQ58: DMA2 Stream 2 global Interrupt
-    Some(DMA2_Stream3_Handler),         // IRQ59: DMA2 Stream 3 global Interrupt
-    Some(DMA2_Stream4_Handler),         // IRQ60: DMA2 Stream 4 global Interrupt
-    Some(ETH_Handler),                  // IRQ61: Ethernet global interrupt
-    Some(ETH_WKUP_Handler),             // IRQ62: Ethernet Wake-up through EXTI line interrupt
-    Some(CAN2_TX_Handler),              // IRQ63: CAN2 TX Interrupt
-    Some(CAN2_RX0_Handler),             // IRQ64: CAN2 RX0 Interrupt
-    Some(CAN2_RX1_Handler),             // IRQ65: CAN2 RX1 Interrupt
-    Some(CAN2_SCE_Handler),             // IRQ66: CAN2 SCE Interrupt
-    Some(OTG_FS_Handler),               // IRQ67: USB OTG FS global Interrupt
-    Some(DMA2_Stream5_Handler),         // IRQ68: DMA2 Stream 5 global Interrupt
-    Some(DMA2_Stream6_Handler),         // IRQ69: DMA2 Stream 6 global Interrupt
-    Some(DMA2_Stream7_Handler),         // IRQ70: DMA2 Stream 7 global Interrupt
-    Some(USART6_Handler),               // IRQ71: USART6 global Interrupt
-    Some(I2C3_EV_Handler),              // IRQ72: I2C3 Event Interrupt
-    Some(I2C3_ER_Handler),              // IRQ73: I2C3 Error Interrupt
-    Some(OTG_HS_EP1_OUT_Handler),       // IRQ74: USB OTG HS End Point 1 Out global Interrupt
-    Some(OTG_HS_EP1_IN_Handler),        // IRQ75: USB OTG HS End Point 1 In global Interrupt
-    Some(OTG_HS_WKUP_Handler),          // IRQ76: USB OTG HS Wakeup through EXTI line
-    Some(OTG_HS_Handler),               // IRQ77: USB OTG HS global Interrupt
-    Some(DCMI_Handler),                 // IRQ78: DCMI global Interrupt
-    Some(CRYP_Handler),                 // IRQ79: CRYP crypto global interrupt
-    Some(HASH_RNG_Handler),             // IRQ80: Hash and Rng global interrupt
-    Some(FPU_Handler),                  // IRQ81: FPU global Interrupt
-    Some(UART7_Handler),                // IRQ82: UART 7 global interrupt
-    Some(UART8_Handler),                // IRQ83: UART 8 global interrupt
-    Some(SPI4_Handler),                 // IRQ84: SPI 4 global interrupt
-    Some(SPI5_Handler),                 // IRQ85: SPI 5 global interrupt
-    Some(SPI6_Handler),                 // IRQ86: SPI 6 global interrupt
-    Some(SAI1_Handler),                 // IRQ87: SAI1 global interrupt
-    Some(LCD_TFT_Handler),              // IRQ88: LTDC global interrupt
-    Some(LCD_TFT_Error_Handler),        // IRQ89: LTDC global Error interrupt
-    Some(DMA2D_Handler),                // IRQ90: DMA2D global interrupt
+    Some(WWDG_Handler),               // IRQ0: Window Watchdog Interrupt
+    Some(PVD_Handler),                // IRQ1: PVD through EXTI Line Detection Interrupt
+    Some(TAMP_STAMP_Handler),         // IRQ2: Tamper and TimeStamp Interrupts
+    Some(RTC_WKUP_Handler),           // IRQ3: RTC Wakeup Interrupt through the EXTI line
+    Some(FLASH_Handler),              // IRQ4: FLASH global Interrupt
+    Some(RCC_Handler),                // IRQ5: RCC global Interrupt
+    Some(EXTI0_Handler),              // IRQ6: EXTI Line0 Interrupt
+    Some(EXTI1_Handler),              // IRQ7: EXTI Line1 Interrupt
+    Some(EXTI2_Handler),              // IRQ8: EXTI Line2 Interrupt
+    Some(EXTI3_Handler),              // IRQ9: EXTI Line3 Interrupt
+    Some(EXTI4_Handler),              // IRQ10: EXTI Line4 Interrupt
+    Some(DMA1_Stream0_Handler),       // IRQ11: DMA1 Stream 0 global Interrupt
+    Some(DMA1_Stream1_Handler),       // IRQ12: DMA1 Stream 1 global Interrupt
+    Some(DMA1_Stream2_Handler),       // IRQ13: DMA1 Stream 2 global Interrupt
+    Some(DMA1_Stream3_Handler),       // IRQ14: DMA1 Stream 3 global Interrupt
+    Some(DMA1_Stream4_Handler),       // IRQ15: DMA1 Stream 4 global Interrupt
+    Some(DMA1_Stream5_Handler),       // IRQ16: DMA1 Stream 5 global Interrupt
+    Some(DMA1_Stream6_Handler),       // IRQ17: DMA1 Stream 6 global Interrupt
+    Some(ADC_Handler),                // IRQ18: ADC1, ADC2 and ADC3 global Interrupts
+    Some(CAN1_TX_Handler),            // IRQ19: CAN1 TX Interrupt
+    Some(CAN1_RX0_Handler),           // IRQ20: CAN1 RX0 Interrupt
+    Some(CAN1_RX1_Handler),           // IRQ21: CAN1 RX1 Interrupt
+    Some(CAN1_SCE_Handler),           // IRQ22: CAN1 SCE Interrupt
+    Some(EXTI9_5_Handler),            // IRQ23: EXTI Line[9:5] Interrupts
+    Some(TIM1_BRK_TIM9_Handler),      // IRQ24: TIM1 Break interrupt and TIM9 global interrupt
+    Some(TIM1_UP_TIM10_Handler),      // IRQ25: TIM1 Update Interrupt and TIM10 global interrupt
+    Some(TIM1_TRG_COM_TIM11_Handler), // IRQ26: TIM1 Trigger and Commutation Interrupts and TIM11 global interrupt
+    Some(TIM1_CC_Handler),            // IRQ27: TIM1 Capture Compare Interrupt
+    Some(TIM2_Handler),               // IRQ28: TIM2 global Interrupt
+    Some(TIM3_Handler),               // IRQ29: TIM3 global Interrupt
+    Some(TIM4_Handler),               // IRQ30: TIM4 global Interrupt
+    Some(I2C1_EV_Handler),            // IRQ31: I2C1 Event Interrupt
+    Some(I2C1_ER_Handler),            // IRQ32: I2C1 Error Interrupt
+    Some(I2C2_EV_Handler),            // IRQ33: I2C2 Event Interrupt
+    Some(I2C2_ER_Handler),            // IRQ34: I2C2 Error Interrupt
+    Some(SPI1_Handler),               // IRQ35: SPI1 global Interrupt
+    Some(SPI2_Handler),               // IRQ36: SPI2 global Interrupt
+    Some(USART1_Handler),             // IRQ37: USART1 global Interrupt
+    Some(USART2_Handler),             // IRQ38: USART2 global Interrupt
+    Some(USART3_Handler),             // IRQ39: USART3 global Interrupt
+    Some(EXTI15_10_Handler),          // IRQ40: EXTI Line[15:10] Interrupts
+    Some(RTC_Alarm_Handler),          // IRQ41: RTC Alarms (A and B) through EXTI Line Interrupt
+    Some(OTG_FS_WKUP_Handler),        // IRQ42: USB OTG FS Wakeup through EXTI line interrupt
+    Some(TIM8_BRK_TIM12_Handler),     // IRQ43: TIM8 Break Interrupt and TIM12 global interrupt
+    Some(TIM8_UP_TIM13_Handler),      // IRQ44: TIM8 Update Interrupt and TIM13 global interrupt
+    Some(TIM8_TRG_COM_TIM14_Handler), // IRQ45: TIM8 Trigger and Commutation Interrupts and TIM14 global interrupt
+    Some(TIM8_CC_Handler),            // IRQ46: TIM8 Capture Compare Interrupt
+    Some(DMA1_Stream7_Handler),       // IRQ47: DMA1 Stream7 global Interrupt
+    Some(FSMC_Handler), // IRQ48: FSMC global Interrupt (Flexible Static Memory Controller)
+    Some(SDIO_Handler), // IRQ49: SDIO global Interrupt
+    Some(TIM5_Handler), // IRQ50: TIM5 global Interrupt
+    Some(SPI3_Handler), // IRQ51: SPI3 global Interrupt
+    Some(UART4_Handler), // IRQ52: UART4 global Interrupt
+    Some(UART5_Handler), // IRQ53: UART5 global Interrupt
+    Some(TIM6_DAC_Handler), // IRQ54: TIM6 global Interrupt and DAC1&2 underrun errors
+    Some(TIM7_Handler), // IRQ55: TIM7 global Interrupt
+    Some(DMA2_Stream0_Handler), // IRQ56: DMA2 Stream 0 global Interrupt
+    Some(DMA2_Stream1_Handler), // IRQ57: DMA2 Stream 1 global Interrupt
+    Some(DMA2_Stream2_Handler), // IRQ58: DMA2 Stream 2 global Interrupt
+    Some(DMA2_Stream3_Handler), // IRQ59: DMA2 Stream 3 global Interrupt
+    Some(DMA2_Stream4_Handler), // IRQ60: DMA2 Stream 4 global Interrupt
+    Some(ETH_Handler),  // IRQ61: Ethernet global interrupt
+    Some(ETH_WKUP_Handler), // IRQ62: Ethernet Wake-up through EXTI line interrupt
+    Some(CAN2_TX_Handler), // IRQ63: CAN2 TX Interrupt
+    Some(CAN2_RX0_Handler), // IRQ64: CAN2 RX0 Interrupt
+    Some(CAN2_RX1_Handler), // IRQ65: CAN2 RX1 Interrupt
+    Some(CAN2_SCE_Handler), // IRQ66: CAN2 SCE Interrupt
+    Some(OTG_FS_Handler), // IRQ67: USB OTG FS global Interrupt
+    Some(DMA2_Stream5_Handler), // IRQ68: DMA2 Stream 5 global Interrupt
+    Some(DMA2_Stream6_Handler), // IRQ69: DMA2 Stream 6 global Interrupt
+    Some(DMA2_Stream7_Handler), // IRQ70: DMA2 Stream 7 global Interrupt
+    Some(USART6_Handler), // IRQ71: USART6 global Interrupt
+    Some(I2C3_EV_Handler), // IRQ72: I2C3 Event Interrupt
+    Some(I2C3_ER_Handler), // IRQ73: I2C3 Error Interrupt
+    Some(OTG_HS_EP1_OUT_Handler), // IRQ74: USB OTG HS End Point 1 Out global Interrupt
+    Some(OTG_HS_EP1_IN_Handler), // IRQ75: USB OTG HS End Point 1 In global Interrupt
+    Some(OTG_HS_WKUP_Handler), // IRQ76: USB OTG HS Wakeup through EXTI line
+    Some(OTG_HS_Handler), // IRQ77: USB OTG HS global Interrupt
+    Some(DCMI_Handler), // IRQ78: DCMI global Interrupt
+    Some(CRYP_Handler), // IRQ79: CRYP crypto global interrupt
+    Some(HASH_RNG_Handler), // IRQ80: Hash and Rng global interrupt
+    Some(FPU_Handler),  // IRQ81: FPU global Interrupt
+    Some(UART7_Handler), // IRQ82: UART 7 global interrupt
+    Some(UART8_Handler), // IRQ83: UART 8 global interrupt
+    Some(SPI4_Handler), // IRQ84: SPI 4 global interrupt
+    Some(SPI5_Handler), // IRQ85: SPI 5 global interrupt
+    Some(SPI6_Handler), // IRQ86: SPI 6 global interrupt
+    Some(SAI1_Handler), // IRQ87: SAI1 global interrupt
+    Some(LCD_TFT_Handler), // IRQ88: LTDC global interrupt
+    Some(LCD_TFT_Error_Handler), // IRQ89: LTDC global Error interrupt
+    Some(DMA2D_Handler), // IRQ90: DMA2D global interrupt
 ];
 
 // Default handler for unused interrupts
