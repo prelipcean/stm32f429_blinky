bceed02d4138b60e5538df5f304b848c147f196a
Gheorghe Prelipcean <prelipcean.gheorghe@yahoo.com>
2025-07-06 11:16:44 +0300

    Added documentation to main.rs file that can be generated by: cargo doc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+//! # stm32f429_blinky
+//!
+//! A minimal bare-metal Rust application for the STM32F429I-DISC1 board.
+//! This file contains the entry point and panic handler.
+
 #![no_std] // Do not link the Rust standard library (required for embedded)
 #![no_main] // Disable all Rust-level entry points
 #![allow(clippy::empty_loop)]
@@ -6,9 +11,13 @@ use core::panic::PanicInfo;
 
 mod startup_stm32f429zi; // MCU-specific startup code
 
-// A mutable static variable
+/// A global mutable counter incremented in the main loop.
 static mut COUNTER: u32 = 42;
 
+/// The main entry point for the application.
+///
+/// # Safety
+/// This function is marked unsafe because it accesses a mutable static variable.
 #[unsafe(no_mangle)]
 fn main() -> ! {
     // This is where our application logic will go.
@@ -22,8 +31,10 @@ fn main() -> ! {
     }
 }
 
+/// Panic handler for the application.
+///
+/// Traps the MCU in an infinite loop on panic.
 #[panic_handler]
 fn panic_handler(_info: &PanicInfo) -> ! {
-    // Trap the MCU in an infinite loop on panic.
     loop {}
 }
